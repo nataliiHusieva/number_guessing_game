76d4cca5a701e831b8ebbad3af78855928ecbaa8
Nataliia Husieva <natalkahusieva@gmail.com>
2025-04-01 22:07:48 +0200

    Easy implementation.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Please, enter your number from 1 to 100.");
+
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1;
+        Scanner scan = new Scanner(System.in);
+        int inputNumber = scan.nextInt();
+
+        if (inputNumber != numberToGuess) {
+            System.out.println("Wrong guess!");
+        } else {
+            System.out.println("You won!");
+        }
     }
 }
